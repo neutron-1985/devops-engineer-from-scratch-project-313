367ccb6a5ef00425535cea01e311434a5d4f2e97
Rasim <neutron-1985@live.ru>
2026-05-01 02:58:48 +0400

    1st step

--- database.py
+++ database.py
@@ -6,13 +6,3 @@ from sqlmodel import SQLModel, create_engine
 load_dotenv()
 
 
-engine = create_engine(
-    os.environ["DATABASE_URL"],
-    echo=True,
-)
-
-
-def init_db():
-    import models  # noqa: F401
-
-    SQLModel.metadata.create_all(engine)
--- models.py
+++ models.py
@@ -1,23 +1,2 @@
 from sqlmodel import Field, SQLModel
 
-
-class LinkBase(SQLModel):
-    original_url: str
-    short_name: str
-
-
-class Link(LinkBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-
-
-class LinkCreate(LinkBase):
-    pass
-
-
-class LinkUpdate(LinkBase):
-    pass
-
-
-class LinkRead(LinkBase):
-    id: int
-    short_url: str
